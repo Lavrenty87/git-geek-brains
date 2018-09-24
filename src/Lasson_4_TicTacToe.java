559bf16498888d9797a0767c0392af914a1d2cdf
Lavreny87 <Lavr19871014@yandex.ru>
2018-09-24 15:27:07 +0300

    Java 1 Home Work 4

--- src/Lasson_4_TicTacToe.java
+++ src/Lasson_4_TicTacToe.java
@@ -1,3 +1,139 @@
-public class Lasson_4_TicTac {
-    
+import java.util.Random;
+import java.util.Scanner;
+
+class Lasson_4_TicTacToe {
+
+    final int SIZE = 6;
+    final char DOT_X = 'x';
+    final char DOT_O = 'o';
+    final char DOT_EMPTY = '.';
+    char[][] map = new char[SIZE][SIZE];
+    Scanner sc = new Scanner(System.in);
+    Random rand = new Random();
+
+    public static void main(String[] args) {
+        new Lasson_4_TicTacToe().game();
+    }
+
+    void game() {
+        initMap();
+        while (true) {
+            humanTurn();
+            if (checkWin(DOT_X)) {
+                System.out.println("YOU WON!");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Sorry, DRAW...");
+                break;
+            }
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("AI WON!");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Sorry, DRAW...");
+                break;
+            }
+        }
+        System.out.println("GAME OVER!");
+        printMap();
+    }
+
+    void initMap() {
+        for (int i = 0; i < SIZE; i++)
+            for (int j = 0; j < SIZE; j++)
+                map[i][j] = DOT_EMPTY;
+    }
+
+    void printMap() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++)
+                System.out.print(map[i][j] + " ");
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Enter X and Y (1..3):");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y));
+        map[y][x] = DOT_X;
+    }
+
+    void aiTurn() {
+        int x, y;
+        do {
+            x = rand.nextInt(SIZE);
+            y = rand.nextInt(SIZE);
+        } while (!isCellValid(x, y));
+        map[y][x] = DOT_O;
+    }
+
+    boolean checkWin(char dot) {
+        // check horizontals
+        int sum_dot = 0;
+        for (int i = 0; i < SIZE; i++) {
+            sum_dot = 0;
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == dot) sum_dot = sum_dot + 1;
+                if (sum_dot == SIZE) return true;
+            }
+        }
+        // check verticals
+        for (int j = 0; j < SIZE; j++) {
+            sum_dot = 0;
+            for (int i = 0; i < SIZE; i++) {
+                if (map[i][j] == dot) sum_dot = sum_dot + 1;
+                if (sum_dot == SIZE) return true;
+            }
+        }
+        // check diagonals
+        sum_dot = 0;
+        for (int j = 0,i = 0; j < SIZE; i++, j++) {
+            if (map[i][j] == dot) sum_dot = sum_dot + 1;
+            if (sum_dot == SIZE) return true;
+        }
+        sum_dot = 0;
+        for (int j = 0, i = SIZE - 1; j < SIZE; i --, j++) {
+            if (map[i][j] == dot) sum_dot = sum_dot + 1;
+            if (sum_dot == SIZE) return true;
+        }
+        return false;
+    }
+
+      /*
+        if (map[0][0] == dot && map[0][1] == dot && map[0][2] == dot) return true;
+        if (map[1][0] == dot && map[1][1] == dot && map[1][2] == dot) return true;
+        if (map[2][0] == dot && map[2][1] == dot && map[2][2] == dot) return true;
+        // check verticals
+        if (map[0][0] == dot && map[1][0] == dot && map[2][0] == dot) return true;
+        if (map[0][1] == dot && map[1][1] == dot && map[2][1] == dot) return true;
+        if (map[0][2] == dot && map[1][2] == dot && map[2][2] == dot) return true;
+        // check diagonals
+        if (map[0][0] == dot && map[1][1] == dot && map[2][2] == dot) return true;
+        if (map[2][0] == dot && map[1][1] == dot && map[0][2] == dot) return true;
+        return false;
+        */
+
+
+    boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++)
+            for (int j = 0; j < SIZE; j++)
+                if (map[i][j] == DOT_EMPTY)
+                    return false;
+        return true;
+    }
+
+    boolean isCellValid(int x, int y) {
+        if (x < 0 || y < 0 || x >= SIZE || y >= SIZE)
+            return false;
+        return map[y][x] == DOT_EMPTY;
+    }
 }
